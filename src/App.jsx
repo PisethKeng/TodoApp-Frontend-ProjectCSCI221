3dbca6f046b651ff3f5b921c0a1dede8437902bb
PisethKeng <piseththebest@gmail.com>
2025-10-19 20:39:08 +0700

    initial commit and environmental setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="flex flex-col justify-center items-center h-screen gap-4">
+    <p className=" text-gray-700 text-3xl text-center">we have tailwind enabled css lesgooooo</p>
+    <a className="text-blue-500 hover:underline" href="https://tailwindcss.com/docs/installation/using-vite" target="_blank">Here is the docs enjoy</a>
+    </div>
+    </>
+  )
+}
+
+export default App
