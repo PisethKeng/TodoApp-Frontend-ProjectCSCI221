c0ddf60b46589f1b609fc03df4351d188cc6f3ac
CHOI HeeJae <chlgmlwo04@gmail.com>
2025-12-01 00:47:21 +0700

    add login route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route, Link } from 'react-router'
 import Home from './pages/Home'
+import Login from "./pages/Login";
 import AddTask from './pages/AddTask'
 import Calendar from './pages/Calendar'
 import Dashboard from './pages/dashboard'
@@ -9,6 +10,7 @@ function App() {
   return (
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/addtask" element={<AddTask />} />
         <Route path="/calendar" element={<Calendar />} />
