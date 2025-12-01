756aea52287b7784b21ba35661206737bd3c61d2
PisethKeng <piseththebest@gmail.com>
2025-12-01 21:20:10 +0700

    Restore app.jsx from recent merges

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,42 @@
 import React from 'react'
 import { Routes, Route, Link } from 'react-router'
 import Home from './pages/Home'
-import Login from "./pages/Login";
 import AddTask from './pages/AddTask'
 import Calendar from './pages/Calendar'
 import Dashboard from './components/dashboard'
 import EditTask from './pages/EditTask'
 import { TaskProvider } from './context/TaskProvider'
+import Login from './pages/Login'
 
 function App() {
   return (
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/addtask" element={<AddTask />} />
-        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/" element={
+          <TaskProvider>
+            <Home />
+          </TaskProvider>
+          } />
+        <Route path="/dashboard" element={
+          <TaskProvider>
+            <Dashboard />
+          </TaskProvider>
+          } />
+        <Route path="/addtask" element={
+          <TaskProvider>
+            <AddTask />
+          </TaskProvider>
+        } />
+        <Route path="/calendar" element={
+          <TaskProvider>
+            <Calendar />
+          </TaskProvider>
+        } />
+        <Route path="/EditTask/:id" element={
+          <TaskProvider>
+            <EditTask />
+          </TaskProvider>
+        } />
       </Routes>
   )
 }
